d27a80eb17521e7b3722b69cbd5a487a1ea5202d
natasajevremov1 <148207412+natasajevremov1@users.noreply.github.com>
2026-07-30 18:43:04 +0200

    Implemented AuthController

---
+++ backend/SmartIntershipSistem/SmartIntershipSistem/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartIntershipSistem.DTOs;
+using SmartIntershipSistem.Services;
+
+namespace SmartIntershipSistem.Controllers
+{
+
+    [ApiController]                 // Enables API behaviors like automatic model validation
+    [Route("auth/[controller]")]     // Sets the route to /api/products
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+
+        }
+
+        [HttpPost("register-candidate")]
+        public async Task<ActionResult<AuthResponseDto>> RegisterCandidate(RegisterCandidateDto registerCandidateDto)
+        {
+            if (registerCandidateDto == null)
+            {
+                return BadRequest();
+            }
+            return Ok(await _authService.RegisterCandidate(registerCandidateDto));
+        }
+
+        [HttpPost("register-company")]
+        public async Task<ActionResult<AuthResponseDto>> RegisterCOmpany(RegisterCompanyDto registerCompanyDto)
+        {
+            if (registerCompanyDto == null) { return BadRequest(); }
+            return Ok(await _authService.RegisterCompany(registerCompanyDto));
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponseDto>> Login(LoginDto login)
+        {
+            if (login == null) { return BadRequest(); }
+            return Ok(await _authService.Login(login));
+
+        }
+    }
+}
